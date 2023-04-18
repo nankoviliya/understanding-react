dcf43ded8205d121671225e85b1a28878bbb7d95
ilis08 <nankoviliya@gmail.com>
2023-04-18 16:07:47 +0300

    Deploy to Github.IO

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <h2>Deploy to Github Pages</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
